54576092a60af13db9b58281479fec48e17d4f78
Maximilian Grau <Maximilian.Grau@st.ovgu.de>
2018-02-21 19:40:25 +0100

    init project

---
+++ TT-CentralUnit/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * TT-CentralUnit.cpp
+ *
+ * Created: 21.02.2018 19:38:36
+ * Author : Maxi
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
